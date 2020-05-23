1f020896d5441c011bb3c5885f6140bb215ecc79
Jason Kao <jason.kao@columbia.edu>
2020-05-22 23:58:09 -0400

    Updates

---
+++ precache-manifest.3732f4c0818770e2a70bbf7aa70586cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38010179a93250ef28f2d905c6cf0a04",
+    "url": "/gerryapp.com/index.html"
+  },
+  {
+    "revision": "ac113f1f43609c981dfb",
+    "url": "/gerryapp.com/static/js/2.5816f758.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gerryapp.com/static/js/2.5816f758.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84799e860f22485495d2",
+    "url": "/gerryapp.com/static/js/main.8d6a2cec.chunk.js"
+  },
+  {
+    "revision": "b3289fe3d150493d43ca",
+    "url": "/gerryapp.com/static/js/runtime-main.75bd4b1f.js"
+  }
+]);
